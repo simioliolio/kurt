c19ac08f4428c1730b599de5f898c51f660dd10c
Simon Haycock <simioliolio@bitbasic.co.uk>
2024-09-11 14:51:19 +0100

    Add sequencer

---
+++ kurt/include/sequencer.hpp
@@ -0,0 +1,29 @@
+#include <unordered_map>
+#include <vector>
+
+namespace kurt {
+
+/**
+ * @brief A sequencer that stores an array of values for each key.
+ */
+template <typename K, typename V> class Sequencer {
+
+public:
+  Sequencer() = default;
+
+  Sequencer(Sequencer const &) = delete;
+  Sequencer(Sequencer &&) = delete;
+  Sequencer &operator=(Sequencer const &) = delete;
+  Sequencer &operator=(Sequencer &&) = delete;
+
+  ~Sequencer() = default;
+
+  void set(K key, std::vector<V> values) noexcept { _sequence[key] = values; }
+
+  std::vector<V> get(K key) const noexcept { return _sequence.at(key); }
+
+private:
+  std::unordered_map<K, std::vector<V>> _sequence;
+};
+
+} // namespace kurt
---
+++ kurt/test/sequencer_test.cpp
@@ -0,0 +1,21 @@
+#include "sequencer.hpp"
+
+#include <array>
+#include <gtest/gtest.h>
+
+using namespace kurt;
+
+class SequencerTest : public ::testing::Test {
+protected:
+  Sequencer<uint16_t, std::string> sequencer;
+  void SetUp() override {}
+};
+
+TEST_F(SequencerTest, SetAndRecall) {
+  sequencer.set(0, {"zero"});
+  sequencer.set(1, {"one"});
+  sequencer.set(2, {"two"});
+  ASSERT_EQ(sequencer.get(0), std::vector<std::string>{"zero"});
+  ASSERT_EQ(sequencer.get(1), std::vector<std::string>{"one"});
+  ASSERT_EQ(sequencer.get(2), std::vector<std::string>{"two"});
+}
