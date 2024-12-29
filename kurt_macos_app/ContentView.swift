ac7951711d965cfcb69fba869fdb61e0ed24334a
Simon Haycock <simioliolio@bitbasic.co.uk>
2024-12-29 12:15:04 +0000

    Add macos app

---
+++ kurt_macos_app/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  KurtDelete
+//
+//  Created by Simon Haycock on 29/12/2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, kurt here!!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
